e4c4cebf1db55c03786d9925adc1cdec2f07673e
po-hsien li <pohsien.ee@gmail.com>
2023-05-26 19:38:08 +0800

    LUT

---
+++ 3bit_LUT.v
@@ -0,0 +1,21 @@
+module top_module (
+    input clk,
+    input enable,
+    input S,
+    input A, B, C,
+    output Z ); 
+    
+    reg [7:0] Q;
+    
+    assign Z = Q[{A,B,C}];
+        
+    always@(posedge clk) begin
+        if (enable) begin
+            Q <= {Q[6:0],S};
+        end
+        else 
+            Q <= Q; 
+    end
+	
+endmodule
+
